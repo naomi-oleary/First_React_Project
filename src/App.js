690924c79744eca81d438903abe63c886f3f9a51
naomi-oleary <oleary.ng@gmail.com>
2026-02-23 22:51:49 -0700

    Next: Featured Books

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Landing from './components/Landing';
+import Nav from './components/Nav';
+import Highlights from './components/Highlights'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Landing />
+      <Highlights />
+    </div>
+  );
+}
+
+export default App;
