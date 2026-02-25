dd45e2bd5f7cb76a22c45fe069bc740f3acf7db9
naomi-oleary <oleary.ng@gmail.com>
2026-02-25 11:34:03 -0700

    fix routing bug

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import Landing from './components/Landing';
 import Nav from './components/Nav';
-import Highlights from './components/Highlights'
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home';
+import Books from './pages/Books';
+import { books } from './data'
+import BookInfo from './pages/BookInfo';
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/books" render={() => <Books books={books} />} />
+          <Route path="/books/1" render={() => <BookInfo books={books} />} />
+        </Switch>
+        <Footer />
     </div>
+    </Router>
   );
 }
 
---
+++ src/pages/BookInfo.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const BookInfo = () => {
+  return (
+    <div id="books_body">
+        <div id="books__main">
+            <div className="books__container">
+                <div className="row">
+                    <div className="book__selected--top">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default BookInfo;
